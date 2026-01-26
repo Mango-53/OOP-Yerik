650aa49ec72e5daaf806018316a7677219db660b
Yerik Lin Hernandez <187869@udla.fundacion.mx>
2026-01-26 16:11:03 -0600

    Primer archivo subido al repositorio

---
+++ Testing1/Testing1/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+int area, sideA, sideB, height;
+
+height = int.Parse(Console.ReadLine());
+sideA = int.Parse(Console.ReadLine());
+sideB = int.Parse(Console.ReadLine());
+
+area = height*sideA*sideB;
+Console.WriteLine("The result is: " + area);
