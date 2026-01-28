ba8f18408368c156d09fc5c1a5ac28462113a200
Yerik Lin Hernandez <187869@udla.fundacion.mx>
2026-01-28 16:36:15 -0600

    Function, Par o Impar

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+int num1;
+Console.WriteLine("Escriba el número");
+num1 = int.Parse(Console.ReadLine());
+bool pairOrImpair(int num1)
+{
+    if (num1 % 2 == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
+}
+
+if (pairOrImpair(num1))
+{
+    Console.WriteLine("El número es par");
+}
+else
+{
+    Console.WriteLine("El número es impar");
+}
