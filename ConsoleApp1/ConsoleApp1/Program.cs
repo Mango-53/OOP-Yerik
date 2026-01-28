f7afac012c77398f8a7831340abf087cd09ae40c
Yerik Lin Hernandez <187869@udla.fundacion.mx>
2026-01-28 15:49:45 -0600

    Tabla de multiplicar de cualquier número

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+    
+int num;
+int table;
+
+num = int.Parse(Console.ReadLine());
+    Console.WriteLine("Escribe el número a multiplicar");
+for (int i = 0; i < 10; i++)
+{
+    table=num*(i+1);
+    Console.WriteLine(table);
+}
